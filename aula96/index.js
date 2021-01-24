2bf87b86894f57abe61fff5c39fc84ae97df5a22
Lucas-Souza-Santos <lucas.ssantos2@ufpe.br>
2021-01-24 12:14:52 -0300

    final da aula 96

---
+++ aula96/index.js
@@ -0,0 +1,18 @@
+/**
+ * teste lógico ? SE verdadeiro: SE falso
+*/
+    let n1 = 10;
+    let n2 = 15; 
+    let op = true; 
+
+    let resultado = n1 > n2 ? "n1 é maior": "n2 é maior";
+
+    console.log(resultado);
+
+    n1 == n2 ? console.log("Igual"): console.log("Diferente");
+
+    const r = op ? "igual": "diferente";
+
+    const r2 = op && "igual";
+
+    console.log(r2);
