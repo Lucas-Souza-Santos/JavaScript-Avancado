40b761fb6264665c8f452aaf603e1ef670ae7bac
Lucas-Souza-Santos <lucas.ssantos2@ufpe.br>
2021-01-30 00:11:12 -0300

    final da aula 105

---
+++ aula106/index.js
@@ -0,0 +1,9 @@
+let curso = "Curso de JavaScript Moderno";
+
+console.log(curso.split(" "));
+// startsWith retorna true se a string começar com o valor passado
+console.log(curso.startsWith("Curso"));
+console.log(curso.toUpperCase());
+// endsWith retorna true se a string terminar com o valor passado
+console.log(curso.endsWith("o"));
+
