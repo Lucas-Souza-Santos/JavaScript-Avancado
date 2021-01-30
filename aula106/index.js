4637f467b02e5f67d1124e8dd5f19ea88deb5bcc
Lucas-Souza-Santos <lucas.ssantos2@ufpe.br>
2021-01-30 00:18:17 -0300

    final da aula 106

--- aula106/index.js
+++ aula106/index.js
@@ -2,8 +2,12 @@ let curso = "Curso de JavaScript Moderno";
 
 console.log(curso.split(" "));
 // startsWith retorna true se a string começar com o valor passado
-console.log(curso.startsWith("Curso"));
+console.log(curso.startsWith("Curso") ? "Verdadeiro":"Falso");
 console.log(curso.toUpperCase());
 // endsWith retorna true se a string terminar com o valor passado
-console.log(curso.endsWith("o"));
+console.log(curso.endsWith("o") ? "Verdadeiro":"Falso");
+// includes verifica em qualquer lugar da string se ela tem o valor passado
+console.log(curso.includes("de") ? "Verdadeiro": "Falso");
+// repeat ela repet a string n vezes
+console.log(curso.repeat(2))
 
