3acbf18fd41f91839a7dc576a0f48bc0e5b742cc
Lucas-Souza-Santos <lucas.ssantos2@ufpe.br>
2021-01-30 01:21:02 -0300

    final da aula 109

---
+++ aula109/index.js
@@ -0,0 +1,13 @@
+let produtos = new Map()
+
+produtos.set("01", "Mouse");
+produtos.set("02", "Teclado");
+produtos.set("03", "Monitor");
+produtos.set("04", "Gabinete");
+
+
+// produtos.clear(); apagar os elementos 
+
+// produtos.delete("03");
+
+console.log(produtos);
